baf022b0ba3b178d24476b34f27e37078f902bb9
EPICGAMING191 <burrj22@outlook.com>
2025-10-15 20:05:48 -0400

    All in one tuning file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jack/Drive/AllInOneTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jack/Drive/AllInOneTuning.java
@@ -1,27 +1,70 @@
 package org.firstinspires.ftc.teamcode.Jack.Drive;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class AllInOneTuning extends OpMode {
     public GamepadV1 gamepad = new GamepadV1();
+    public MecanumDriveOnly mecDrive = new MecanumDriveOnly();
+
+
     public enum modeSelected {
         SELECTED,
         NOT_SELECTED
     }
+
+    public enum modes {
+        DRIVE,
+        SHOOT,
+        INTAKE,
+        CAMERA
+    }
+
     public modeSelected isModeSelected = modeSelected.NOT_SELECTED;
+    public modes mode = modes.DRIVE;
+
+    public int selectedMode = 0;
 
     @Override
     public void init() {
         gamepad.init(gamepad1, 0.3);
-        if(isModeSelected == modeSelected.NOT_SELECTED){
-            if(gamepad.isGamepadReady()){
+        mecDrive.init(hardwareMap, gamepad);
 
-            }
-        }
     }
 
     @Override
     public void loop() {
+        if(isModeSelected == modeSelected.NOT_SELECTED) {
+            menuSelectUpdate();
+        }
+        else {
+            tuningUpdate();
+        }
+    }
 
+    public void menuSelectUpdate(){
+        telemetry.addData("Selected tuning mode: ", mode.name());
+        if(gamepad.isGamepadReady()) {
+            if (gamepad.gamepad.dpad_up) {
+                mode = modes.values()[Math.max(0, (selectedMode - 1))];
+            } else if (gamepad.gamepad.dpad_down) {
+                mode = modes.values()[Math.min((selectedMode - 1), modes.values().length - 1)];
+            }
+            if (gamepad.gamepad.circle) {
+                isModeSelected = modeSelected.SELECTED;
+            }
+        }
+    }
+
+    public void tuningUpdate(){
+        switch (mode){
+            case DRIVE:
+                mecDrive.drive();
+                mecDrive.log(telemetry);
+                break;
+            case SHOOT:
+                break;
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jack/Drive/MecanumDriveOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jack/Drive/MecanumDriveOnly.java
@@ -1,16 +1,21 @@
 package org.firstinspires.ftc.teamcode.Jack.Drive;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class MecanumDriveOnly extends OpMode {
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class MecanumDriveOnly {
     private DcMotor frontLeftMotor;
     private DcMotor frontRightMotor;
     private DcMotor backLeftMotor;
     private DcMotor backRightMotor;
     public RobotConstantsV1 constants = new RobotConstantsV1();
+    public GamepadV1 gamepad1 = new GamepadV1();
 
-    @Override
-    public void init() {
+    public void init(HardwareMap hardwareMap, Gamepad gamepad) {
+        gamepad1.init(gamepad, 0.3);
         frontLeftMotor = hardwareMap.get(DcMotor.class, constants.frontLeft);
         frontRightMotor = hardwareMap.get(DcMotor.class, constants.frontRight);
         backLeftMotor = hardwareMap.get(DcMotor.class, constants.backLeft);
@@ -22,15 +27,25 @@ public class MecanumDriveOnly extends OpMode {
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void loop() {
-        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-        double x = -gamepad1.left_stick_x; // Counteract imperfect strafing, if the back motors are facing downwards this should be negative
-        double rx = -gamepad1.right_stick_x; //This is reversed for our turning
-        drive(y, x, rx);
+    public void init(HardwareMap hardwareMap, GamepadV1 gamepad) {
+        this.gamepad1 = gamepad;
+        frontLeftMotor = hardwareMap.get(DcMotor.class, constants.frontLeft);
+        frontRightMotor = hardwareMap.get(DcMotor.class, constants.frontRight);
+        backLeftMotor = hardwareMap.get(DcMotor.class, constants.backLeft);
+        backRightMotor = hardwareMap.get(DcMotor.class, constants.backRight);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
 
-    public void drive(double y, double x, double rx) {
+
+    public void drive() {
+        double y = -gamepad1.gamepad.left_stick_y; // Remember, this is reversed!
+        double x = -gamepad1.gamepad.left_stick_x; // Counteract imperfect strafing, if the back motors are facing downwards this should be negative
+        double rx = -gamepad1.gamepad.right_stick_x; //This is reversed for our turning
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double frontLeftPower = (-y + x + rx) / denominator;
         double backLeftPower = (-y - x + rx) / denominator;
@@ -41,5 +56,16 @@ public class MecanumDriveOnly extends OpMode {
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
+    }
+
+    public void log(Telemetry telemetry){
+        telemetry.addLine("Front Left Power: " + frontLeftMotor.getPower());
+        telemetry.addLine("Front Right Power: " + frontRightMotor.getPower());
+        telemetry.addLine("Back Left Power: " + backLeftMotor.getPower());
+        telemetry.addLine("Back Right Power: " + backRightMotor.getPower());
+        telemetry.addLine("Front Left Position: " + frontLeftMotor.getCurrentPosition());
+        telemetry.addLine("Front Right Position: " + frontRightMotor.getCurrentPosition());
+        telemetry.addLine("Back Left Position: " + backLeftMotor.getCurrentPosition());
+        telemetry.addLine("Back Right Position: " + backRightMotor.getCurrentPosition());
     }
 }
