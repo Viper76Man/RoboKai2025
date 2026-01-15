ce186712975d7225cc788b44bc26788777e768d2
Viper76Man <nick@bind-tech.com>
2026-01-15 13:34:36 -0500

    ELC Encoder test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/EncoderTest.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.Coach;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
+@Disabled
 @Autonomous(name="Encoder Test")
 
 public class EncoderTest extends LinearOpMode {
