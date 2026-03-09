354cf9c26ac40da6c45cadd0f259aee06658bc67
Viper76Man <nick@bind-tech.com>
2026-03-09 12:36:43 -0400

    Updated Next for hardware

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Coach/BasicDriveTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.Coach;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.MotorEx;
+
+@TeleOp(name = "NextFTC Drive Test")
+public class BasicDriveTest extends NextFTCOpMode {
+    public BasicDriveTest() {
+        addComponents(
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+    // change the names and directions to suit your robot
+    private final MotorEx frontLeftMotor = new MotorEx("fl");
+    private final MotorEx frontRightMotor = new MotorEx("fr");
+    private final MotorEx backLeftMotor = new MotorEx("bl");
+    private final MotorEx backRightMotor = new MotorEx("br");
+
+    @Override
+    public void onStartButtonPressed() {
+        Command driverControlled = new MecanumDriverControlled(
+                frontLeftMotor,
+                frontRightMotor,
+                backLeftMotor,
+                backRightMotor,
+                Gamepads.gamepad1().leftStickY(),
+                Gamepads.gamepad1().leftStickX(),
+                Gamepads.gamepad1().rightStickX()
+        );
+        driverControlled.schedule();
+    }
+}
